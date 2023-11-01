024870df1b3647d2b8bfbd299e51c61951114393
Pijay <53463564+yopijay@users.noreply.github.com>
2023-11-01 21:32:50 +0800

    Final
    - Remove unnecessary codes and files

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -5,13 +5,3 @@ import Env from "./Env.json";
 const env = 'local' // Environment
 
 export const countries = async () => { return await api({ url: `${Env[env].url}/countries`, method: `get` }).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const authentication = async data => { return await api({ url: `${Env[env].url}/login`, method: 'post', data: data }).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const profile = async id => { return await api({ url: `${Env[env].url}/profile/${id}`, method: 'get'}).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const records = async ({ table, data }) => { return await api({ url: `${Env[env].url}/list/${table}`, method: 'post', data: data }).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const series = async table => { return await api({ url: `${Env[env].url}/series/${table}`, method: 'get' }).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const specific = async ({ table, id }) => { return await api({ url: `${Env[env].url}/specific/${table}/${id}`, method: 'get' }).then(res => res.data); }
-// export const save = async ({ table, data }) => { return await api({ url: `${Env[env].url}/save/${table}`, method: 'post', data: data }).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const update = async ({ table, data }) => { return await api({ url: `${Env[env].url}/update/${table}`, method: 'post', data: data }).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const look = async ({ table, data }) => { return await api({ url: `${Env[env].url}/search/${table}`, method: 'post', data: data }).then(res => res.data); }
-// export const history = async ({ table, data }) => { return await api({ url: `${Env[env].url}/logs/${table}`, method: 'post', data: data }).then(res => res.data).catch(err => console.log(`Error: ${err}`)); }
-// export const dropdown = async ({ table, data }) => { return await api({ url: `${Env[env].url}/dropdown/${table}`, method: 'post', data: data }).then(res => res.data); }
--- src/core/components/loader/Screen.jsx
+++
@@ -1,3 +0,0 @@
-import Logo from 'assets/images/logo.png'; // Assets
-const Screen = () => { return <img alt= "logo" src= { Logo } width= "40px" height= "auto" /> }
-export default Screen;
